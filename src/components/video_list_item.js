912007ba3864b49b577fc829023ea0860cea1bab
Nhan <nhansphotoblog@gmail.com>
2017-05-30 15:27:05 -0700

    update video list item to display thumbnail and title

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const VideoListItem = ({video}) => { //{video} is shortcut - this object has a property called video, please grab that video and assign it to variable called video
-  console.log(video);
-  return <li>Video</li>
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return(
+    <li className="list-group-item">
+      <div className="video-list media">
+
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+
+      </div>
+    </li>
+  );
 };
 
 // const VideoListItem = (props) => {
