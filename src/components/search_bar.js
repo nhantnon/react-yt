c7424982a69958de0c2d3c7bb550e4ec2f321571
Nhan <nhansphotoblog@gmail.com>
2017-05-30 17:29:48 -0700

    add search functionality for user input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -24,7 +24,7 @@ class SearchBar extends React.Component{
        <div className="search-bar">
        <input
           value = {this.state.term}
-          onChange={(event) => this.setState({ term: event.target.value})} />
+          onChange={(event) => this.onInputChange(event.target.value)} />
      </div>
 
       // //<input onChange={this.onInputChange} />;
@@ -39,6 +39,11 @@ class SearchBar extends React.Component{
   // onInputChange(event) {
   //   console.log(event.target.value)
   // }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
 
 
